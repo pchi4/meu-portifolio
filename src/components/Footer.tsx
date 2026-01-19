a7d6bf2d13b816ff5157d685d0c67b1cb0629286
Douglas Lopes <douglas.santos@ssp.ba.gov.br>
2026-01-19 13:07:14 -0300

    refactor: remove unecessaty components

--- src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export function Footer() {
-  return (
-    <footer className="bg-black text-slate-500 text-center py-6 text-sm">
-      © {new Date().getFullYear()} Douglas Lopes — Todos os direitos reservados
-    </footer>
-  );
-}
